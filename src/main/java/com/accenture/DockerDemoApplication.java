74b0e4d64b152b1051066c25b5bb7776c5fdeaf3
Huril <hemanta.devi.huril@accenture.com>
2018-07-11 15:44:14 +0400

    fourth

--- src/main/java/com/accenture/DockerDemoApplication.java
+++ src/main/java/com/accenture/DockerDemoApplication.java
@@ -2,11 +2,20 @@ package com.accenture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DockerDemoApplication {
+public class DockerDemoApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DockerDemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerDemoApplication.class, args);
 	}
+	
+
 }
