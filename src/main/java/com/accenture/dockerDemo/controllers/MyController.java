14f682c0b7bf5edadada1e9b5a103ae6983cecc4
Huril <hemanta.devi.huril@accenture.com>
2018-07-11 14:09:46 +0400

    first

---
+++ src/main/java/com/accenture/dockerDemo/controllers/MyController.java
@@ -0,0 +1,22 @@
+package com.accenture.dockerDemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+	
+	@GetMapping("/")
+	public String goToHomePage() {
+		return "home";
+	}
+	
+	@GetMapping("/processing")
+	public String loadPage(Model model, @RequestParam("name") String name) {
+		model.addAttribute("name", name);
+		return "home";
+	}
+	
+}
